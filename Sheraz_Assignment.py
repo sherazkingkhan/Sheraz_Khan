38f31bd12511f4ed928c940313c64d1615f9f21f
sherazkingkhan <156561931+sherazkingkhan@users.noreply.github.com>
2024-01-21 19:26:51 +0500

    Add files via upload

---
+++ Sheraz_Assignment.py
@@ -0,0 +1,63 @@
+#SHERAZ KHAN (sherazking006@gmail.com) 0346-3174279
+
+#Answer 1.
+num1 = 10
+num2 = 5
+sum_result = num1 + num2
+print("The sum of", num1, "and", num2, "is:", sum_result)
+
+#Answer:2.
+message = "Hello"
+message += " World!"
+print(message)
+
+#Answer: 3.
+is_python_fun = True
+if is_python_fun:
+    print("Python is fun!")
+else:
+    print("not fun")
+
+#Answer: 4.
+fruits = ["Apple", "Banana", "Orange"]
+print("Original list of fruits:", fruits)
+fruits.append("Grapes")
+print("Updated list of fruits:", fruits)
+
+#Answer: 5.
+price = 19.99
+converted_price = int(price)
+print("Original price:", price)
+print("Converted price:", converted_price)
+
+#Answer: 6.
+student_info = {'name': 'Shiraz Khan','age': 20,'grade': 'A'}
+print("Student Information:")
+print(student_info)
+
+#Answer:7.
+user_age = int(input("Enter your age: "))
+if 0 <= user_age <= 12:
+print("You are a Child.")
+elif 13 <= user_age <= 19:
+print("You are a Teenager.")
+elif 20 <= user_age <= 64:
+print("You are an Adult.")
+else:
+print("You are a Senior Citizen.")
+
+#Answer: 8.
+comp_num = 2 + 3j
+print("Real part:", comp_num.real)
+print("Imaginary part:", comp_num.imag)
+
+#Answer : 9.
+string1 = "Hello"
+string2 = " World!"
+combined_string = string1 + string2
+print(f"The combined string is: {combined_string}. Its length is {len(combined_string)} characters.")
+
+#Answer : 10.
+days_of_week = ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday')
+third_day = days_of_week[2]
+print("The third day of the week is:", third_day)
